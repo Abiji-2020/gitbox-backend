b4ebc9eada9f2fa7d09ccb9d09393b4a830168f4
Abinand P <abinand0911@gmail.com>
2024-07-13 21:28:18 +0530

    initialization of project

---
+++ src/test/java/gitbox/GitboxApplicationTests.java
@@ -0,0 +1,13 @@
+package gitbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
