def6175551f71dec51f3d881195cbc9b8d309200
Abinand P <abinand0911@gmail.com>
2024-07-24 20:07:26 +0530

    Removed Spring Security and having a test get Mapping

--- src/main/java/gitbox/GitboxApplication.java
+++ src/main/java/gitbox/GitboxApplication.java
@@ -6,11 +6,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class GitboxApplication {
 
-	
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitboxApplication.class, args);
